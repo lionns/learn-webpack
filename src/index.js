246f9e381f6b4e9ec19fc4c9122f825ddcee770f
lionns <juans.leonv@icloud.com>
2022-01-20 23:49:19 -0500

    Hola

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Template from './templates/Template.js';
 import '@styles/main.css';
 
 
+console.log('Hola')
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
